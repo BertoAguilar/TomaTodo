881d012d98ed8043e8332b798a43d523e682e687
Alberto <albertoxaguilar10@gmail.com>
2024-11-30 15:40:41 -0800

    first commit

---
+++ src/main/java/com/alberto/TomaTodo/TomaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.TomaTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomaTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alberto/TomaTodo/TomaTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.TomaTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomaTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
